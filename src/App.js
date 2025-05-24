be775262029e9203921323be1df8ce00a681606d
Sripathy R S <sripathysathya04@gmail.com>
2025-05-24 14:12:31 +0530

    ss

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import NavBar from './NavBar/NavBar';
 import About from './NavBar/About';
 import Achievements from './NavBar/Achiements'
 import ContactUs from './NavBar/ContactUs';
-import Login from './NavBar/Login';
 import Footer from './NavBar/Footer';
 import './NavBar/NavBar.css'
-import Signup from './NavBar/Signup';
 import Projects from './NavBar/Projects';
 
 import { Link } from 'react-router-dom';
@@ -24,8 +22,6 @@ function App() {
             <Route path="/About" element={<About/>}/>
             {/* <Route path="/Achievements" element={<Achievements/>}/> */}
             <Route path="/ContactUs" element={<ContactUs/>}/>
-            <Route path="/Login" element={<Login/>}/> 
-            <Route path="/Signup" element={<Signup/>}/>    
             <Route path="/Projects" element={<Projects/>}/>     
           </Routes>
           <Footer />   
